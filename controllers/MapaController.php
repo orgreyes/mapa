3aa598f318f3f6b9db2826a80a1e64ce4bf44d4e
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-09-08 20:55:25 -0600

    Se agrego el contenido JS.

--- controllers/MapaController.php
+++ controllers/MapaController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Mapa;
+use Exception;
 
 class MapaController {
     public static function index(Router $router){
@@ -14,6 +15,11 @@ class MapaController {
     public static function buscarAPI(){
         $mapa_nombre = $_GET['mapa_nombre'] ?? '';
         $sql = "SELECT * FROM mapas WHERE mapa_situacion = '1'";
+    
+        if (!empty($mapa_nombre)) {
+            $mapa_nombre = strtolower($mapa_nombre);
+            $sql .= " AND LOWER(mapa_nombre) LIKE '%$mapa_nombre%' ";
+        }
     
         try {
             $mapas = Mapa::fetchArray($sql); 
---
+++ src/js/mapa/index.js
@@ -0,0 +1,87 @@
+import Swal from "sweetalert2";
+import { Toast } from "../funciones";
+import L from "leaflet";
+
+const btnActualizar = document.getElementById("btnActualizar");
+
+const map = L.map('mapa', {
+    center: [0, 0], 
+    zoom: 2, 
+    minZoom: 2, 
+    maxZoom: 10,
+});
+
+map.on('resize', function () {
+    map.invalidateSize();
+});
+
+const mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10, 
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const markerLayer = L.layerGroup();
+const icono = L.icon({
+    iconUrl: './images/ubicacion.png',
+    iconSize: [35, 35]
+});
+
+L.circle([15.52, -90.32], { radius: 5000 }).addTo(markerLayer);
+
+markerLayer.addTo(map);
+
+const buscar = async () => {
+    const url = `/mapa/API/mapa/buscar`;
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        console.log(data);
+
+        if (data && data.length > 0) {
+            data.forEach(registro => {
+                const latitud = parseFloat(registro.mapa_latitud);
+                const longitud = parseFloat(registro.mapa_longitud);
+
+                if (!isNaN(latitud) && !isNaN(longitud)) {
+                    const NuevoMarcador = L.marker([latitud, longitud], {
+                        icon: icono,
+                        draggable: true
+                    });
+
+                    const popup = L.popup()
+                        .setLatLng([latitud, longitud])
+                        .setContent(`<p>Nombre: ${registro.mapa_nombre}</p>`);
+
+                    NuevoMarcador.bindPopup(popup);
+                    NuevoMarcador.addTo(markerLayer);
+                }
+            });
+        } else {
+            Toast.fire({
+                title: 'No se encontraron registros',
+                icon: 'info'
+            });
+        }
+    } catch (error) {
+        console.error('Error al cargar los datos desde la base de datos:', error);
+    }
+};
+
+btnActualizar.addEventListener("click", () => {
+    Toast.fire({
+        title: 'Actualizando datos...',
+        icon: 'info',
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 750
+    });
+    buscar();
+});
+
+btnActualizar.click();
