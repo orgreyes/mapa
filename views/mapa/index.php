78b4197c667abdb2be5dc738ae42ee8e1440467a
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-09-08 20:32:46 -0600

    Se le agrego estilos css al index.php de viws

--- views/mapa/index.php
+++ views/mapa/index.php
@@ -3,6 +3,29 @@
 <head>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css">
 </head>
+<style>
+        .container {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .map-container {
+            margin-top: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            height: 400px;
+            overflow: hidden;
+        }
+
+        #btnActualizar {
+            background-color: #3498db;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding: 5px 15px;
+            cursor: pointer;
+        }
+    </style>
 <body>
     <div class="container">
         <h1>MAPA CON COORDENADAS REGISTRADAS</h1>
